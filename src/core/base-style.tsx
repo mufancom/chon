c6ca5f9f0b251abf8b06f627502568357f1cd3a8
yggdrasilqh <yggdrasilqh@gmail.com>
2018-08-27 16:25:11 +0800

    remove console.log

--- src/core/base-style.tsx
+++ src/core/base-style.tsx
@@ -79,8 +79,6 @@ export class StyleProvider extends React.Component<StyleProviderProps> {
     return (
       <StyleContextConsumer>
         {context => {
-          console.log({context});
-
           if (!context) {
             context = this.buildContext();
           }
@@ -88,20 +86,12 @@ export class StyleProvider extends React.Component<StyleProviderProps> {
           let {schema, schemas: schemaDict, mapping, mappingConfig} = context;
 
           let {schema: schemaName, children} = this.props;
-          console.log({schemaName});
 
           if (schemaName) {
             schemaName = mapping[schemaName];
             schema = schemaDict[schemaName];
             mapping = {...mapping, ...mappingConfig[schemaName]};
           }
-
-          console.log({
-            schema,
-            schemas: schemaDict,
-            mapping,
-            mappingConfig,
-          });
 
           return (
             <StyleContextProvider
